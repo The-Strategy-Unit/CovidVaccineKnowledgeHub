886b98b40aabb3a19683107c1db7030a8bf30548
Tom Jemmett <thomas.jemmett@nhs.net>
2021-12-22 15:01:14 +0000

    removes shinydashboard dependency

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,7 +3,6 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
-#' @import shinydashboard
 #' @noRd
 app_ui <- function(request) {
   header <- bs4Dash::dashboardHeader(
