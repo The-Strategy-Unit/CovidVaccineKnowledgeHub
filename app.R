3b3294b194d427c68f3c309713f26d2de634103b
Tom Jemmett <thomas.jemmett@nhs.net>
2022-01-17 16:38:36 +0000

    disables shiny autoload

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+golem::disable_autoload()
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
+options("golem.app.prod" = TRUE)
 CovidVaccineKnowledgeHub::run_app() # add parameters here (if any)
