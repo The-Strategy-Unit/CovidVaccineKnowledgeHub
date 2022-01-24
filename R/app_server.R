1870e4297f55464f5a54f33e0d20c60588681463
Tom Jemmett <thomas.jemmett@nhs.net>
2022-01-24 21:09:40 +0000

    fixes bug in the initial observer that populates the sidebar

--- R/app_server.R
+++ R/app_server.R
@@ -9,60 +9,58 @@ app_server <- function( input, output, session ) {
 
   # on initialisation:
   # load excel file
-  d <- load_data()
+  data <- reactive({
+    load_data()
+  })
 
   # update inputs
   # populate evidence
 
   observe({
-    f <- function(x) {
-      v <- sort(unique(d[[x]]))
-      updateSelectizeInput(session, x, choices = v)
-    }
-    f("main_theme")
-    f("clinic_model")
-    f("methods_used")
+    d <- data()
+
+    methods_used <- sort(unique(d[["methods_used"]]))
+    updateSelectizeInput(session, "methods_used", choices = methods_used)
 
     date <- sort(unique(d$date))
     date <- purrr::set_names(date, format(date, "%b-%y"))
     updateSelectizeInput(session, "dates", choices = date)
 
     tags <- sort(unique(purrr::flatten_chr(d[["tags"]])))
-    updateSelectizeInput(session, tags, choices = tags)
+    updateSelectizeInput(session, "tags", choices = tags)
   })
 
   # outputs
   filtered_data <- reactive({
-    data <- d
+    d <- data()
 
     if (isTruthy(input$search)) {
       # TODO: replace regex with levenshtein?
-      data <- data |>
+      d <- d |>
         dplyr::filter(dplyr::if_any(where(is.character), stringr::str_detect, stringr::fixed(input$search)))
     }
 
     if (isTruthy(input$dates)) {
-      data <- data |>
+      d <- d |>
         dplyr::filter(.data[["date"]] %in% as.POSIXct(input$dates, tz = "UTC"))
     }
 
     if (isTruthy(input$level_evidence)) {
-      data <- data |>
+      d <- d |>
         dplyr::filter(.data[["level_evidence"]] %in% input$level_evidence)
     }
 
     if (isTruthy(input$methods_used)) {
-      data <- data |>
+      d <- d |>
         dplyr::filter(.data[["methods_used"]] %in% input$methods_used)
-
     }
 
     if (isTruthy(input$jcvi_cohort)) {
-      data <- data |>
+      d <- d |>
         dplyr::filter(purrr::map_lgl(.data[["jcvi_cohort"]], ~any(.x %in% input$jcvi_cohort)))
     }
 
-    data
+    d
   })
 
   mod_knowledge_items_server("evidence", filtered_data)
